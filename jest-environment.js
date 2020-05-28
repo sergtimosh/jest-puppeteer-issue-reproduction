60bf9945a124ca37fcfe76858cca45236b814ecb
sergtimosh <sergtimosh@gmail.com>
2020-05-28 20:56:54 +0300

    - small changes - add hook failure event

--- jest-environment.js
+++ jest-environment.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const path = require('path');
 const PuppeteerEnvironment = require('jest-environment-puppeteer');
 require('jest-circus');
@@ -10,19 +9,19 @@ class JestEnvironment extends PuppeteerEnvironment {
     await super.setup()
     //https://stackoverflow.com/questions/49934994/parallel-support-using-jest-with-puppeteer
     //https://stackoverflow.com/questions/48317725/is-it-safe-to-run-multiple-instances-of-puppeteer-at-the-same-time
-    // await this.global.browser.createIncognitoBrowserContext()
-    // await this.global.jestPuppeteer.debug()
-    //await this.global.page._client.send('Network.clearBrowserCookies'); // clears cookies before test
+    const incognitoContext = await this.global.browser.createIncognitoBrowserContext()
+    this.global.page = await incognitoContext.newPage()
+    // await this.global.page._client.send('Network.clearBrowserCookies'); // clears cookies before test
     this.global.page.setDefaultTimeout(10000)
   }
 
   async teardown() {
-    await new Promise(resolve => setTimeout(resolve, 1800));
+    // await new Promise(resolve => setTimeout(resolve, 1800)) - after updated of jest and Jest Circus timeout not needed
     await super.teardown()
   }
 
   async handleTestEvent(event, state) {
-    const retryAttempts = 2;
+    //test failure
     if (event.name === 'test_fn_failure') {
       if (state.currentlyRunningTest.invocations > 0) {
         const testName = state.currentlyRunningTest.name;
@@ -38,9 +37,25 @@ class JestEnvironment extends PuppeteerEnvironment {
         } catch (e) {
           console.log(e)
         }
-
       }
     }
+    //hook failure
+    if (event.name === 'hook_failure') {
+      const testName = 'hook_failure';
+      const screenshotsFolder = path.resolve(__dirname + '/reports/screenshots/');
+      const toFilename = s => s.replace(/[^a-z0-9.-]+/gi, '_');
+      const tzoffset = (new Date()).getTimezoneOffset() * 60000
+      const localISOTime = (new Date(Date.now() - tzoffset)).toISOString()
+      const filePath = path.join(screenshotsFolder,
+        toFilename(`${localISOTime}_${testName}.png`))
+
+      try {
+        await this.global.page.screenshot({ path: filePath })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
   }
 }
 
