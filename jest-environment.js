91793c6998d9ca028724a22a0fdf740571f74e1d
sergtimosh <sergtimosh@gmail.com>
2020-09-17 13:46:13 +0300

    add method to expand page to viewer

--- jest-environment.js
+++ jest-environment.js
@@ -1,13 +1,13 @@
-const PuppeteerEnvironment = require('jest-environment-puppeteer');
-
+const PuppeteerEnvironment = require("jest-environment-puppeteer");
 
 class JestEnvironment extends PuppeteerEnvironment {
 
   async setup() {
     await super.setup()
     //to fix issue uncomment next two lines
-    // const incognitoContext = await this.global.browser.createIncognitoBrowserContext()
-    // this.global.page = await incognitoContext.newPage()
+    const incognitoContext = await this.global.browser.createIncognitoBrowserContext()
+    this.global.page = await incognitoContext.newPage()
+    this.global.page.bringToFront()
 
   }
 
